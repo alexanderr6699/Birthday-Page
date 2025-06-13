849c0f32f279b54580fc21ff655360889096cf7e
alexanderr6699 <crypto.miner02579@gmail.com>
2025-06-13 13:13:54 +0530

    Add files via upload

---
+++ mumu.js
@@ -0,0 +1,4 @@
+// Handle Surprise Gift button click
+document.getElementById("btn__gift").addEventListener("click", function () {
+    window.location.href = "surprise.html"; // Change this to your actual page path
+});
